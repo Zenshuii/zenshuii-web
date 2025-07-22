4f5adaa7f57570e7a73894acb871835678cbbf3f
SMelidoni <simone.melidoni@gmail.com>
2025-07-22 22:13:03 +0100

    feat: add dummy stoic page

---
+++ src/app/stoic/page.tsx
@@ -0,0 +1,14 @@
+import { Navbar } from '@/components/Navbar';
+
+export default function StoicAppPage() {
+  return (
+    <>
+      <Navbar />
+      <main className="flex min-h-screen items-center justify-center">
+        <h1 className="text-2xl font-bold text-[#FFB877]">
+          Stoic App Page (Dummy)
+        </h1>
+      </main>
+    </>
+  );
+}
