d590a70c3e39bf91ba49c2420ffe0141f0a76247
SMelidoni <simone.melidoni@gmail.com>
2025-07-22 22:16:29 +0100

    feat: add dummy contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,14 @@
+import { Navbar } from '@/components/Navbar';
+
+export default function ContactPage() {
+  return (
+    <>
+      <Navbar />
+      <main className="flex min-h-screen items-center justify-center">
+        <h1 className="text-2xl font-bold text-[#FFB877]">
+          Contact Page (Dummy)
+        </h1>
+      </main>
+    </>
+  );
+}
