5623fa1d0d11f2102d8e20e44a22d84ae277c016
SMelidoni <simone.melidoni@gmail.com>
2025-07-22 22:08:03 +0100

    feat: add dummy about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+import { Navbar } from '@/components/Navbar';
+
+export default function AboutPage() {
+  return (
+    <>
+      <Navbar />
+      <main className="flex min-h-screen items-center justify-center">
+        <h1 className="text-2xl font-bold text-[#FFB877]">
+          About Page (Dummy)
+        </h1>
+      </main>
+    </>
+  );
+}
