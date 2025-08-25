a8b62e153dc9aeb9d3c7bb116d08ee33d20ca6f0
Simone Melidoni <simone.melidoni@outlook.com>
2025-08-25 19:02:01 +0100

    feature/remove-test-app-references (#12)
    * feat: remove test app from nav links
    
    * feat: remove test app from apps meta list
    
    * feat: remove test app page component

--- src/data/apps.ts
+++ src/data/apps.ts
@@ -17,12 +17,4 @@ export const apps: AppMeta[] = [
     image: '/apps/stoic-thumb.png',
     tagline: 'Mindful Stoicism',
   },
-  {
-    slug: 'test',
-    name: 'Test App',
-    description:
-      'Experimental sandbox for prototyping future mindful tools and UI patterns.',
-    href: '/apps/test',
-    tagline: 'R&D Playground',
-  },
 ];
