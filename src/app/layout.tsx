12b98af62f0532b06e6ff471d1e84eaebbe4cbed
SMelidoni <simone.melidoni@gmail.com>
2025-08-30 11:42:59 +0100

    style: initial setup for sticky footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
       <body
         className={`${poppins.variable} flex min-h-screen flex-col antialiased`}>
         <Navbar />
-        <main className="flex-1">{children}</main>
+        <main className="flex flex-1 flex-col justify-center">{children}</main>
         <Footer />
       </body>
     </html>
